1062e25c9edd6268aaceba5e9efd9047af842f1d
Jessica <35634254+jessna96@users.noreply.github.com>
2022-12-22 12:11:37 +0100

    added logic

--- script.js
+++ script.js
@@ -1,7 +1,78 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = {
+    rock: 'Rock',
+    paper: 'Paper',
+    scissors: 'Scissors'
+}
+const gameOutcome = {
+    win: 1,
+    loose: -1,
+    draw: 0
+}
+const gameOutcomeToMessage = {
+    [gameOutcome.win]: 'win',
+    [gameOutcome.draw]: 'draw',
+    [gameOutcome.loose]: 'loose',
+}
+const rules = {
+    [choices.rock]: {
+        [choices.rock]: gameOutcome.draw,
+        [choices.paper]: gameOutcome.loose,
+        [choices.scissors]: gameOutcome.win
+    },
+    [choices.paper]: {
+        [choices.rock]: gameOutcome.win,
+        [choices.paper]: gameOutcome.draw,
+        [choices.scissors]: gameOutcome.loose
+    },
+    [choices.scissors]: {
+        [choices.rock]: gameOutcome.loose,
+        [choices.paper]: gameOutcome.win,
+        [choices.scissors]: gameOutcome.draw
+    },
+
+}
+const predAndMessage = [[(score) => score > 0, 'You win!'], [(score) => score < 0, 'You loose!'], [(score) => score === 0, 'Draw!']];
+
+const choicesToGameOutcome = (a, b) => rules[a][b];
+
+const getRandomInteger = (max) => Math.floor(Math.random() * max)
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)];
+    return Object.values(choices)[getRandomInteger(3)];
 }
 
-console.log(getComputerChoice());
+const range = (min, max) => {
+    const arr = [];
+    for (let i = min; i < max; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+function sum(list) {
+    return list.reduce(add);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function game() {
+    const scores = range(0, 5).map(() => playRound(getComputerChoice()));
+    return predAndMessage.find(([pred]) => pred(sum(scores)))[1];
+
+}
+
+console.log(game());
+
+function playRound(computerSelection) {
+    const playerSelection = capitalize(prompt('Rock, Paper or Scissors?'));
+    const outcome = choicesToGameOutcome(playerSelection, computerSelection);
+    console.log(gameOutcomeToMessage[outcome]);
+    return outcome;
+}
+
+function capitalize(string) {
+    const lowerCaseStr = string.toLowerCase();
+    return lowerCaseStr.replace(lowerCaseStr[0], lowerCaseStr[0].toUpperCase());
+}
